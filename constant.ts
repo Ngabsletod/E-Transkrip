43f245922ac3581fa0f68069dabd8ff6ca4d9b17
Ngabsletod <143369970+Ngabsletod@users.noreply.github.com>
2025-12-20 11:11:10 +0700

    Add files via upload

---
+++ constant.ts
@@ -0,0 +1,87 @@
+
+import { Course } from './types';
+
+export const COURSES: Course[] = [
+  // Semester 1
+  { id: "TKS25101", code: "TKS25101", name: "Matematika I", sks: 4, semester: 1 },
+  { id: "TKS25102", code: "TKS25102", name: "Fisika Dasar – Mekanika", sks: 2, semester: 1 },
+  { id: "TKS25103", code: "TKS25103", name: "Algoritma dan Pemrograman", sks: 2, semester: 1 },
+  { id: "TKS25104", code: "TKS25104", name: "Geologi dan Geotektonik", sks: 2, semester: 1 },
+  { id: "TKS25105", code: "TKS25105", name: "Gambar Struktur Bangunan", sks: 3, semester: 1 },
+  { id: "TKS25106", code: "TKS25106", name: "Analisis Struktur I", sks: 3, semester: 1 },
+  { id: "TKS25107", code: "TKS25107", name: "Geomatika", sks: 1, semester: 1 },
+  { id: "TKS25108", code: "TKS25108", name: "Praktikum Geomatika", sks: 1, semester: 1 },
+  { id: "UNO2010", code: "UNO2010", name: "Jati Diri Unwiku", sks: 2, semester: 1 },
+
+  // Semester 2
+  { id: "TKS25201", code: "TKS25201", name: "Matematika II", sks: 3, semester: 2 },
+  { id: "TKS25202", code: "TKS25202", name: "Kimia", sks: 3, semester: 2 },
+  { id: "TKS25203", code: "TKS25203", name: "Fisika Dasar – Listrik dan Termal", sks: 2, semester: 2 },
+  { id: "TKS25204", code: "TKS25204", name: "Analisis Struktur II", sks: 3, semester: 2 },
+  { id: "TKS25205", code: "TKS25205", name: "Rekayasa Lingkungan", sks: 2, semester: 2 },
+  { id: "TKS25206", code: "TKS25206", name: "Mekanika Fluida", sks: 2, semester: 2 },
+  { id: "TKS25207", code: "TKS25207", name: "Rekayasa Lalu Lintas", sks: 2, semester: 2 },
+  { id: "TKS25208", code: "TKS25208", name: "Mekanika Tanah", sks: 3, semester: 2 },
+
+  // Semester 3
+  { id: "TKS25301", code: "TKS25301", name: "Matematika III", sks: 3, semester: 3 },
+  { id: "TKS25302", code: "TKS25302", name: "Biologi Lingkungan & Ekologi", sks: 2, semester: 3 },
+  { id: "TKS25303", code: "TKS25303", name: "Mekanika Bahan", sks: 3, semester: 3 },
+  { id: "TKS25304", code: "TKS25304", name: "Bahan Bangunan dan Dasar-Dasar Teknologi Beton", sks: 3, semester: 3 },
+  { id: "TKS25305", code: "TKS25305", name: "Geometri Jalan Raya", sks: 2, semester: 3 },
+  { id: "TKS25306", code: "TKS25306", name: "Rekayasa Pondasi", sks: 4, semester: 3 },
+  { id: "TKS25307", code: "TKS25307", name: "Praktikum Mekanika Tanah", sks: 1, semester: 3 },
+  { id: "TKS25308", code: "TKS25308", name: "Struktur Kayu dan Bambu", sks: 2, semester: 3 },
+  { id: "TKS25309", code: "TKS25309", name: "Hidraulika", sks: 1, semester: 3 },
+  { id: "TKS25310", code: "TKS25310", name: "Praktikum Hidraulika", sks: 1, semester: 3 },
+
+  // Semester 4
+  { id: "TKS25401", code: "TKS25401", name: "Metode Numerik", sks: 3, semester: 4 },
+  { id: "TKS25402", code: "TKS25402", name: "Statistik dan Probabilitas", sks: 2, semester: 4 },
+  { id: "TKS25403", code: "TKS25403", name: "Analisis Struktur III", sks: 2, semester: 4 },
+  { id: "TKS25404", code: "TKS25404", name: "Struktur Baja I", sks: 2, semester: 4 },
+  { id: "TKS25405", code: "TKS25405", name: "Struktur Beton Bertulang I", sks: 3, semester: 4 },
+  { id: "TKS25406", code: "TKS25406", name: "Bahan Perkerasan", sks: 2, semester: 4 },
+  { id: "TKS25407", code: "TKS25407", name: "Manajemen Konstruksi", sks: 3, semester: 4 },
+  { id: "TKS25408", code: "TKS25408", name: "Hidrologi Terapan", sks: 1, semester: 4 },
+  { id: "TKS25409", code: "TKS25409", name: "Praktikum Teknologi Bahan & Beton", sks: 1, semester: 4 },
+  { id: "TKS25410", code: "TKS25410", name: "Praktikum Hidrologi", sks: 1, semester: 4 },
+  { id: "TKS25411", code: "TKS25411", name: "Bahasa Inggris Teknik", sks: 2, semester: 4 },
+
+  // Semester 5
+  { id: "TKS25501", code: "TKS25501", name: "Matematika IV", sks: 2, semester: 5 },
+  { id: "TKS25502", code: "TKS25502", name: "Struktur Baja II", sks: 3, semester: 5 },
+  { id: "TKS25503", code: "TKS25503", name: "Struktur Beton Bertulang II", sks: 3, semester: 5 },
+  { id: "TKS25504", code: "TKS25504", name: "Rekayasa Irigasi", sks: 3, semester: 5 },
+  { id: "TKS25505", code: "TKS25505", name: "Drainase", sks: 2, semester: 5 },
+  { id: "TKS25506", code: "TKS25506", name: "Metode Pelaksanaan Konstruksi", sks: 2, semester: 5 },
+  { id: "TKS25507", code: "TKS25507", name: "K3 Konstruksi & Manajemen Risiko", sks: 2, semester: 5 },
+  { id: "TKS25508", code: "TKS25508", name: "Praktikum Bahan Perkerasan", sks: 1, semester: 5 },
+  { id: "UNO3011", code: "UNO3011", name: "Kewirausahaan", sks: 3, semester: 5 },
+
+  // Semester 6
+  { id: "TKS25601", code: "TKS25601", name: "Aplikasi Komputer Teknik Sipil", sks: 2, semester: 6 },
+  { id: "TKS25602", code: "TKS25602", name: "Perancangan Bangunan Sipil I (Capstone)", sks: 2, semester: 6 },
+  { id: "UNO2001", code: "UNO2001", name: "Pendidikan Pancasila", sks: 2, semester: 6 },
+  { id: "TKSP2501", code: "TKSP2501", name: "Analisis Dinamika Struktur & Teknik Gempa", sks: 2, semester: 6 },
+  { id: "TKSP2502", code: "TKSP2502", name: "Beton Pracetak & Prategang", sks: 2, semester: 6 },
+  { id: "TKSP2503", code: "TKSP2503", name: "Metode Elemen Hingga", sks: 2, semester: 6 },
+  { id: "TKSP2505", code: "TKSP2505", name: "Perencanaan & Permodelan Transportasi", sks: 2, semester: 6 },
+  { id: "TKSP2507", code: "TKSP2507", name: "Jalan Rel", sks: 2, semester: 6 },
+
+  // Semester 7
+  { id: "TKS25701", code: "TKS25701", name: "Metode Penelitian", sks: 2, semester: 7 },
+  { id: "TKS25702", code: "TKS25702", name: "Etika Profesi & Tanggung Jawab Insinyur", sks: 2, semester: 7 },
+  { id: "TKS25703", code: "TKS25703", name: "Perancangan Bangunan Sipil II (Capstone)", sks: 2, semester: 7 },
+  { id: "UNO2002", code: "UNO2002", name: "Pendidikan Kewarganegaraan", sks: 2, semester: 7 },
+  { id: "UNO2003", code: "UNO2003", name: "Bahasa Indonesia", sks: 2, semester: 7 },
+  { id: "UNO2004", code: "UNO2004", name: "Pendidikan Agama", sks: 2, semester: 7 },
+  { id: "TKSP2504", code: "TKSP2504", name: "Struktur Jembatan", sks: 2, semester: 7 },
+
+  // Semester 8
+  { id: "TA801", code: "TA801", name: "Kerja Praktik / KP", sks: 3, semester: 8 },
+  { id: "TA802", code: "TA802", name: "KKN (Kuliah Kerja Nyata)", sks: 3, semester: 8 },
+  { id: "TA803", code: "TA803", name: "Tugas Akhir", sks: 6, semester: 8 },
+  { id: "TS801", code: "TS801", name: "Seminar Teknik Sipil", sks: 2, semester: 8 },
+  { id: "TS802", code: "TS802", name: "Industri 4.0 dalam Konstruksi", sks: 1, semester: 8 }
+];
